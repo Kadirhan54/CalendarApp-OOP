822b1b7d2839f729446bf203ef95c43a1a495fdc
Kadirhan54 <kadirhansaglam5454@gmail.com>
2023-09-25 18:36:00 +0300

    Add project files.

---
+++ CalendarApp/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using CalendarApp.Entities;
+
+Meeting meeting= new Meeting()
+{
+    Title = "Title", 
+    Details = "Details",
+    StartTime= DateTime.Now,
+    EndTime= DateTime.Now,
+    Guests = new() {"guest1" , "guest2" },
+};
+
+Todo Todo= new Todo() 
+{ 
+    Title= "Title",
+    Details = "Details",
+    StartTime= DateTime.Now,
+    EndTime= DateTime.Now ,
+    Importance="HIGH"
+};
